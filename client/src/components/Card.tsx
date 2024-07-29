b191408386eb96d2ee190c3555937be1700f1f4d
Sofialv05 <sofalvsy@gmail.com>
2024-07-29 23:46:15 +0700

    fix: fix card

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -2,11 +2,7 @@ export default function Card() {
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
       <figure className="px-10 pt-10">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
-          className="rounded-xl"
-        />
+        <div>IMAGE</div>
       </figure>
       <div className="card-body items-center text-center">
         <h2 className="card-title">Shoes!</h2>

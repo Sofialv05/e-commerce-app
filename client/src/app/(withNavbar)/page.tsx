9d52aa06b1314bc993838354a8f9ad1e56108a21
Sofialv05 <sofalvsy@gmail.com>
2024-07-29 23:43:55 +0700

    feat: card on homepage

--- client/src/app/(withNavbar)/page.tsx
+++ client/src/app/(withNavbar)/page.tsx
@@ -1,8 +1,13 @@
+import Card from "@/components/Card";
+import Carousel from "@/components/Carousel";
+
 export default function Home() {
   return (
     <main>
       <div>
+        <Carousel />
         <h1>Test Home</h1>
+        <Card />
       </div>
     </main>
   );
---
+++ client/src/components/Carousel.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Carousel() {
+  return (
+    <>
+      <div className="carousel w-full">Carousel</div>
+    </>
+  );
+}

a058171993d6027aa753c88dadf99fc98ff42c8c
Sofialv05 <sofalvsy@gmail.com>
2024-07-31 07:27:09 +0700

    fix: product interface on model

--- client/src/db/models/Product.ts
+++ client/src/db/models/Product.ts
@@ -1,9 +1,22 @@
 import { ProductData } from "@/interfaces/ProductData";
 import DB from "../connection";
 
+interface Product {
+  name: string;
+  slug: string;
+  description: string;
+  excerpt: string;
+  price: number;
+  tags: string[];
+  thumbnail: string;
+  images: string[];
+  createdAt: string;
+  updatadAt: string;
+}
+
 class ProductModel {
   static async findAllProducts() {
-    const collection = DB.collection<ProductData>("Products");
+    const collection = DB.collection<Product>("Products");
 
     return collection.find().toArray();
   }
